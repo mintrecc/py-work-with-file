9d40e3afddf8952dfbad8880118c8725d78d8ea9
mintrecc <maxdavydenkooo@gmail.com>
2026-08-23 14:09:43 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report_dict = dict()
+    with (open(data_file_name, "r") as f):
+
+        for line in f:
+            key, amount = line.split(",")
+            report_dict[key] = report_dict.get(key, 0) + int(amount)
+
+    report_dict["result"] = report_dict["supply"] - report_dict["buy"]
+
+    with open(report_file_name, "w") as f:
+        freez_order = ("supply", "buy", "result")
+        for key in freez_order:
+            f.write(f"{key},{report_dict[key]}\n")
